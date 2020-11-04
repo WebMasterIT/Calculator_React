10f665f94a32d41dd6ec8030cb12b9f0a35eb23c
WebMasterIT! <wspc777@yandex.ru>
2020-11-04 16:16:19 +0300

    Add homepage

---
+++ build/precache-manifest.570f6bbf0bbaeda2ada56af351d54a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7908e692b29d25a6641a66669a367af",
+    "url": "/Calculator_React/index.html"
+  },
+  {
+    "revision": "5a211639f29e2c888e55",
+    "url": "/Calculator_React/static/css/main.9a6bc5bd.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/Calculator_React/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator_React/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a211639f29e2c888e55",
+    "url": "/Calculator_React/static/js/main.13c370b2.chunk.js"
+  },
+  {
+    "revision": "066e786eb1ec3a8e6ba7",
+    "url": "/Calculator_React/static/js/runtime-main.9cf01d00.js"
+  }
+]);
